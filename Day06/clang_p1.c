201143de06a0712aa23fbbaf70386bfc893d3fe5
Wirgiliusz <kamx98@gmail.com>
2022-12-06 14:10:10 +0100

    Add solution for day 6 part 1

--- Day06/clang_p1.c
+++ Day06/clang_p1.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUF_SIZE 128
+// Bad hard-coded solution
+// But fast to implement
+int last_four_different(char *arr) {
+    if (arr[0] != arr[1] &&
+        arr[0] != arr[2] &&
+        arr[0] != arr[3]) {
+
+        if (arr[1] != arr[2] &&
+            arr[1] != arr[3]) {
+            
+            if (arr[2] != arr[3]) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
 
 int main() {
-    int result = 0;
-
     FILE *fd = fopen("input.txt", "r");
     if (!fd) {
         puts("Error: Can not open the file!\n");
         return 1;
     }
 
-    char buf[BUF_SIZE];
-    while (fgets(buf, BUF_SIZE, fd) != NULL)  {
-
+    char last_four[4] = {0};
+    char c;
+    int i = 0;
+    while ((c = fgetc(fd)) != EOF) {
+        last_four[i % 4] = c;
+        if (last_four_different(last_four) && i > 3) {
+            break;
+        }
+    
+        i++;
     }
 
 
-    printf("Result: %d\n", result);
+    printf("Result: %d\n", i+1);
     return 0;
 }
