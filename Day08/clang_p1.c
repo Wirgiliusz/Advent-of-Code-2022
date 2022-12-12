94e8c337c74462810081b3cb375b4f8308977fd2
Wirgiliusz <kamx98@gmail.com>
2022-12-12 21:23:45 +0100

    Add solution for day 8 part 2

--- Day08/clang_p1.c
+++ Day08/clang_p1.c
@@ -2,9 +2,21 @@
 #include <string.h>
 
 #define BUF_SIZE 128
+#define GRID_SIZE 99
 
 int main() {
     int result = 0;
+    int grid[GRID_SIZE][GRID_SIZE];
+    int visibility_grid[GRID_SIZE][GRID_SIZE];
+    for (int row=0; row<GRID_SIZE; row++) {
+        for (int col=0; col<GRID_SIZE; col++) {
+            if (row == 0 || row == GRID_SIZE-1 || col == 0 || col == GRID_SIZE-1) {
+                visibility_grid[row][col] = 1;
+            } else {
+                visibility_grid[row][col] = 0;
+            }
+        }
+    }
 
     FILE *fd = fopen("input.txt", "r");
     if (!fd) {
@@ -13,8 +25,75 @@ int main() {
     }
 
     char buf[BUF_SIZE];
+    int row = 0;
     while (fgets(buf, BUF_SIZE, fd) != NULL)  {
+        for (int col=0; col<GRID_SIZE; col++) {
+            grid[row][col] = buf[col] - '0';
+        }
 
+        row++;
+    }
+
+    // for (int row=0; row<GRID_SIZE; row++) {
+    //     for (int col=0; col<GRID_SIZE; col++) {
+    //         printf("%d", grid[row][col]);
+    //     }
+    //     printf("\n");
+    // }
+
+    int max_height = 0;
+
+    // Left to Right
+    for (int row=1; row<GRID_SIZE; row++) {
+        max_height = 0;
+        for (int col=0; col<GRID_SIZE; col++) {
+            if (grid[row][col] > max_height) {
+                max_height = grid[row][col];
+                visibility_grid[row][col] += 1;
+            }
+        }
+    }
+
+    // Right to Left
+    for (int row=1; row<GRID_SIZE; row++) {
+        max_height = 0;
+        for (int col=GRID_SIZE-1; col>=0; col--) {
+            if (grid[row][col] > max_height) {
+                max_height = grid[row][col];
+                visibility_grid[row][col] += 1;
+            }
+        }
+    }
+
+    // Up to Down
+    for (int col=1; col<GRID_SIZE; col++) {
+        max_height = 0;
+        for (int row=0; row<GRID_SIZE; row++) {
+            if (grid[row][col] > max_height) {
+                max_height = grid[row][col];
+                visibility_grid[row][col] += 1;
+            }
+        }
+    }
+
+    // Down to Up
+    for (int col=1; col<GRID_SIZE; col++) {
+        max_height = 0;
+        for (int row=GRID_SIZE-1; row>=0; row--) {
+            if (grid[row][col] > max_height) {
+                max_height = grid[row][col];
+                visibility_grid[row][col] += 1;
+            }
+        }
+    }
+
+
+    for (int row=0; row<GRID_SIZE; row++) {
+        for (int col=0; col<GRID_SIZE; col++) {
+            if (visibility_grid[row][col] > 0) {
+                result++;
+            }
+        }
     }
 
 
